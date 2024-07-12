610c51556c1fa44210cdc59d6a0b8dcc4293993a
AndreyZagrebelnyy <zaganddmi@gmail.com>
2024-07-12 08:58:42 +0300

    buttons

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -16,47 +16,41 @@ import VotesList from '../page/votes/VotesList';
 
 
 function App() {
-  const [user, setUser] = useState()
+	const [user, setUser] = useState()
 
-  const AxiosChekUser = async () => {
-    const { data } = await requestAxios.get('/tokens/refresh');
+	const AxiosChekUser = async () => {
+		const { data } = await requestAxios.get('/tokens/refresh');
 
-    setUser(data.user);
-    setAccessToken(data.accessToken);
+		setUser(data.user);
+		setAccessToken(data.accessToken);
 
-  };
-  useEffect(() => {
-
-
-    AxiosChekUser();
-  }, [])
+	};
+	useEffect(() => {
 
 
-  return (
-    <>
-      <Navbar user={user} setUser={setUser} />
-      <Routes>
-        <Route path='/' element={<Main />} />
+		AxiosChekUser();
+	}, [])
 
-        <Route path='/initiatives' element={<Initiatives user={user} setUser = {setUser} />} />
 
-        <Route path='/votes' element={<VotesList  />} />
-
-    
-
-        <Route
-          path='/registration'
-          element={<Registration setUser={setUser} user={user} />}
-        />
-        <Route
-          path='/authorization'
-          element={<Authorization setUser={setUser} user={user} />}
-        />
-        <Route path='*' element={<h1>404</h1>} />
-      </Routes>
-
-    </>
-  )
+	return (
+		<>
+			<Navbar user={user} setUser={setUser} />
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/initiatives' element={<Initiatives user={user} setUser={setUser} />} />
+				<Route path='/votes' element={<VotesList />} />
+				<Route
+					path='/registration'
+					element={<Registration setUser={setUser} user={user} />}
+				/>
+				<Route
+					path='/authorization'
+					element={<Authorization setUser={setUser} user={user} />}
+				/>
+				<Route path='*' element={<h1>404</h1>} />
+			</Routes>
+		</>
+	)
 
 }
 
